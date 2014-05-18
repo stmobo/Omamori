8ec0623dfd05202304b73cb19af4613bbcdaa561
KillaVanilla/Tatantyler/-1 Up <tatantyler@yahoo.com>
2014-05-18 16:23:09 -0500

    Start work on scheduler / multitasking
    The low-level context switcher's been written. Now I have to write the
    higher-level scheduler.

--- src/arch/x86/multitask.cpp
+++ src/arch/x86/multitask.cpp
@@ -3,19 +3,16 @@
 #include "includes.h"
 #include "arch/x86/multitask.h"
 #include "arch/x86/table.h"
+#include "core/scheduler.h"
 
+uint32_t syscall_num;
 cpu_regs active_regs;
 size_t current_kern_stack = 0;
 // __syscall_entry will load this on context switch and reload from this when returning.
 
-// Context control flow:
-// Process does an int 0x5C or is preempted.
-// it moves all of its registers (except %esp, %ebp, and segment registers) to active_regs.
-// 
+// active_tss->esp0 must be loaded upon scheduling a process to be run.
 
 extern "C" {
-    uint32_t do_syscall();
-    void do_context_switch();
     void __usermode_jump(size_t func, size_t cs, size_t ds, size_t stack_addr);
 }
 
@@ -26,27 +23,24 @@ uint32_t do_syscall() {
 }
 
 void do_context_switch() {
-    kprintf("Ctext switch!");
+    kprintf("Context switch!");
+    if( syscall_num ) {
+        // we can reenable interrupts, since we're not going to the scheduler
+        asm volatile("sti" : : : "memory");
+        uint32_t ret = do_syscall();
+        active_regs.eax = ret;
+        return;
+    } else {
+        // we have to keep interrupts disabled in the scheduler.
+        process next_process = process_scheduler();
+        // load context
+        active_tss->esp0 = next_process->kernel_stack;
+        active_regs = next_process->regs;
+    }
+    // interrupts are enabled after the IRET in multitask_ll.s
 }
-
-/*
-void do_context_switch( process *p ) {
-    active_regs.eax = p->regs.eax;
-    active_regs.ebx = p->regs.ebx;
-    active_regs.ecx = p->regs.ecx;
-    active_regs.edx = p->regs.edx;
-    active_regs.esi = p->regs.esi;
-    active_regs.edi = p->regs.edi;
-    active_regs.eflags = p->regs.eflags;
-    active_regs.eip = p->regs.eip;
-    asm volatile("mov %0, %%cr3" : : "r"(p->pd) : "memory");
-}
-*/
 
 void switch_kern_stack() {
-    if( current_kern_stack != 0 ) {
-        k_vmem_free( current_kern_stack );
-    }
     current_kern_stack = k_vmem_alloc( UMODE_STACK_PAGES );
     active_tss.esp0 = current_kern_stack + (UMODE_STACK_PAGES*0x1000) - 1;
     active_tss.ss0 = GDT_KDATA_SEGMENT*0x08;
--- src/arch/x86/multitask_ll.s
+++ src/arch/x86/multitask_ll.s
@@ -1,19 +1,30 @@
 # multitask_ll.s
 
+.global syscall_num
 .global active_regs
 .global __syscall_entry
-.global __ctext_switch_entry
+.global __multitasking_kmode_entry
 .global __usermode_jump
 
-# struct order:
-# uint32_t eax;
-# uint32_t ebx;
-# uint32_t ecx;
-# uint32_t edx;
-# uint32_t esi;
-# uint32_t edi;
-# uint32_t eip;
-# uint32_t eflags;
+#typedef struct cpu_regs {
+#    uint32_t eax;     // 0
+#    uint32_t ebx;     // 4
+#    uint32_t ecx;     // 8
+#    uint32_t edx;     // 12
+#    uint32_t esi;     // 16
+#    uint32_t edi;     // 20
+#    uint32_t eip;     // 24
+#    uint32_t eflags;  // 28
+#    uint32_t esp;     // 32
+#    uint32_t ebp;     // 36
+#    uint16_t cs;      // 40
+#    uint16_t ds;      // 42
+#    uint16_t es;      // 44
+#    uint16_t fs;      // 46
+#    uint16_t gs;      // 48
+#    uint32_t cr3;     // 50
+#    uint32_t kern_esp;// 54
+#} cpu_regs;
 
 # ... program stack ...
 # Saved EFLAGS
@@ -21,145 +32,108 @@
 # Saved EIP
 
 __syscall_entry:
+    push %ebx
+    mov $syscall_num, %ebx
+    mov %eax, (%ebx)
+    pop %ebx
+    # now fall through to below
+    
+__multitasking_kmode_entry:
+    # we are working off of esp0 / ss0 in active_tss.
     mov %eax, (active_regs)
     mov $active_regs, %eax
-    # push "easy" registers
+    # save "easy" registers
     mov %ebx, 4(%eax)
     mov %ecx, 8(%eax)
     mov %edx, 12(%eax)
     mov %esi, 16(%eax)
     mov %edi, 20(%eax)
     mov %ebp, 36(%eax)
+    mov %cr3, %ebx
+    mov %ebx, 50(%eax)
     
-    push %ebx
+    # save EFLAGS first
+    mov 12(%esp), %ebx
+    mov %ebx, 28(%eax)
     
+    # make sure we don't get preempted
+    cli
+    
+    # save CS
+    mov 8(%esp), %ebx
+    mov %ebx, 40(%eax)
+    
+    # save other segment registers
+    mov %ds, 42(%eax)
+    mov %es, 44(%eax)
+    mov %fs, 46(%eax)
+    mov %gs, 48(%eax)
+
     # save EIP
     mov 4(%esp), %ebx
     mov %ebx, 24(%eax)
     
-    # save EFLAGS 
-    mov 12(%esp), %ebx
-    mov %ebx, 28(%eax)
-    
     # save ESP (pre-interrupt)
     mov %esp, %ebx
     add $16, %ebx
-    
     mov %ebx, 32(%eax)
     
-    pop %ebx
-    
-    # load data segment registers for kernel mode
     mov $0x10, %eax
     mov %eax, %ds
     mov %eax, %es
     mov %eax, %fs
     mov %eax, %gs
-    
-    call do_syscall
-    mov %eax, (active_regs)
-    mov $active_regs, %eax
-    
-    # reload easy registers
-    mov 4(%eax), %ebx
-    mov 8(%eax), %ecx
-    mov 12(%eax), %edx
-    mov 16(%eax), %esi
-    mov 20(%eax), %edi
-    mov 32(%eax), %esp
-    mov 36(%eax), %ebp
-    
-    # load data segment registers for user mode
-    mov $0x23, %eax
-    mov %eax, %ds
-    mov %eax, %es
-    mov %eax, %fs
-    mov %eax, %gs
-    mov $active_regs, %eax
-    
-    # load EIP for iret
-    push %ebx
-    mov 24(%eax), %ebx
-    mov %ebx, 4(%esp)
-    
-    # load EFLAGS for iret
-    mov 28(%eax), %ebx
-    mov %ebx, 12(%esp)
-    pop %ebx
-    
-    # load EAX
-    mov (active_regs), %eax
-    
-    iret
-    
-__ctext_switch_entry:
-    mov %eax, (active_regs)
-    mov $active_regs, %eax
-    # push "easy" registers
-    mov %ebx, 4(%eax)
-    mov %ecx, 8(%eax)
-    mov %edx, 12(%eax)
-    mov %esi, 16(%eax)
-    mov %edi, 20(%eax)
-    mov %ebp, 36(%eax)
-    
-    push %ebx
-    
-    # save EIP
-    mov 4(%esp), %ebx
-    mov %ebx, 24(%eax)
-    
-    # save EFLAGS 
-    mov 12(%esp), %ebx
-    mov %ebx, 28(%eax)
-    
-    # save ESP (pre-interrupt)
-    mov %esp, %ebx
-    add $16, %ebx
-    
-    mov %ebx, 32(%eax)
-    
-    pop %ebx
-    
-    # load data segment registers for kernel mode
-    mov $0x10, %eax
-    mov %eax, %ds
-    mov %eax, %es
-    mov %eax, %fs
-    mov %eax, %gs
-    
+
     call do_context_switch
+    # were we in a syscall?
+    mov $syscall_num, %eax
+    mov (%eax), %ebx
+    cmp $0, %ebx
+    jne .__skip_syscall_clear
+    
+    # if so, then clear the number
+    mov $0, %ebx
+    mov %ebx, (%eax)
+    
+.__skip_syscall_clear:
     mov $active_regs, %eax
     
-    # reload easy registers
+    # load ESP / EBP
+    mov 32(%eax), %esp
+    mov 36(%eax), %ebp
+    
+    # load EIP for iret
+    mov 24(%eax), %ebx
+    mov %ebx, 4(%esp)
+    
+    # load CS for iret
+    mov $0, %ebx
+    mov 40(%eax), %bx
+    mov %ebx, 8(%esp)
+    
+    # load EFLAGS for iret (this will also reenable interrupts if they were before the context switch)
+    mov 28(%eax), %ebx
+    mov %ebx, 12(%esp)
+    
+    # load cr3
+    mov 50(%eax), %ebx
+    mov %ebx, %cr3
+    
+    # reload general registers
     mov 4(%eax), %ebx
     mov 8(%eax), %ecx
     mov 12(%eax), %edx
     mov 16(%eax), %esi
     mov 20(%eax), %edi
-    mov 32(%eax), %esp
-    mov 36(%eax), %ebp
     
-    # load data segment registers for user mode
-    mov $0x23, %eax
-    mov %eax, %ds
-    mov %eax, %es
-    mov %eax, %fs
-    mov %eax, %gs
-    mov $active_regs, %eax
-    
-    # load EIP for iret
-    push %ebx
-    mov 24(%eax), %ebx
-    mov %ebx, 4(%esp)
-    
-    # load EFLAGS for iret
-    mov 28(%eax), %ebx
-    mov %ebx, 12(%esp)
-    pop %ebx
+    # load segment registers
+    mov 44(%eax), %es
+    mov 46(%eax), %fs
+    mov 48(%eax), %gs
     
     # load EAX
     mov (active_regs), %eax
+    mov (active_regs+42), %ds
     
     iret
 
